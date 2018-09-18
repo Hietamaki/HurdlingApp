be29bc43c0a4025d1b7af90b8ade5d9e567d0d60
KristianWahlroos <kristian.whl@gmail.com>
2018-09-18 11:05:40 +0300

    Swap VideoView to ExoPlayer. Add draggable markers for where to cut the video. #2

--- app/src/main/java/com/example/sloth/hurdlingapp/VideoActivity.java
+++ app/src/main/java/com/example/sloth/hurdlingapp/VideoActivity.java
@@ -3,6 +3,7 @@ package com.example.sloth.hurdlingapp;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
@@ -10,21 +11,57 @@ import android.view.ViewGroup.LayoutParams;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import com.edmodo.rangebar.RangeBar;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
 public class VideoActivity extends Activity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        DefaultTrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+        SimpleExoPlayer player =
+                ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        PlayerView playerView = findViewById(R.id.playerView_step_video);
+        playerView.setPlayer(player);
 
-        VideoView videoView = findViewById(R.id.video_view);
+// Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this,"com.example.sloth.hurdlingapp"), (DefaultBandwidthMeter)bandwidthMeter);
+
+
         String videoPath = DataHolder.Instance.getVideoPath();
-        Uri uri = Uri.parse(videoPath);
-        videoView.setVideoURI(uri);
+        Uri mp4VideoUri = Uri.parse(videoPath);
 
-        MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
 
+// This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(mp4VideoUri);
+// Prepare the player with the source.
+        player.prepare(videoSource);
+
+
+
+        RangeBar rangeBar = findViewById(R.id.rangebar);
+        rangeBar.setTickCount(600);
+        rangeBar.setTickHeight(0);
     }
 }
